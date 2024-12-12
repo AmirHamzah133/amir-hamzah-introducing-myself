137532ee53b14b8134c57de0d3a6816a0b574448
AmirHamzah133 <amirnation23@gmail.com>
2024-12-12 22:15:51 +0700

    add link

--- JS/script.js
+++ JS/script.js
@@ -4,14 +4,13 @@ const arrowR = document.getElementById("arrowRight");
 const arrowL = document.getElementById("arrowLeft");
 
 arrowR.addEventListener("click", function(){
-    menuFloat.classList.remove("right-4")
-    menuFloat.translate.valueOf(40)
+    menuFloat.classList.add("translate-x-40")
     arrowR.classList.toggle("hidden")
     arrowL.classList.remove("hidden")
 })
 
 arrowL.addEventListener("click", function(){
-    menuFloat.classList.toggle("right-4");
+    menuFloat.classList.remove("translate-x-40")
     arrowL.classList.toggle("hidden");
     arrowR.classList.remove("hidden")
 })
