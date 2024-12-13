390b1663b5cee09102018451df70ab6ae0426576
AmirHamzah133 <amirnation23@gmail.com>
2024-12-13 08:15:31 +0700

    add drop menu

--- JS/script.js
+++ JS/script.js
@@ -2,15 +2,27 @@ const menuFloat = document.getElementById("floatingMenu");
 const menu = document.getElementById("hamburgerMenu");
 const arrowR = document.getElementById("arrowRight");
 const arrowL = document.getElementById("arrowLeft");
+const dropMenu = document.getElementById("menuDrop");
 
 arrowR.addEventListener("click", function(){
-    menuFloat.classList.add("translate-x-40")
-    arrowR.classList.toggle("hidden")
-    arrowL.classList.remove("hidden")
+    menuFloat.classList.add("translate-x-40");
+    arrowR.classList.toggle("hidden");
+    arrowL.classList.remove("hidden");
 })
 
 arrowL.addEventListener("click", function(){
-    menuFloat.classList.remove("translate-x-40")
+    menuFloat.classList.remove("translate-x-40");
     arrowL.classList.toggle("hidden");
-    arrowR.classList.remove("hidden")
+    arrowR.classList.remove("hidden");
+})
+
+menu.addEventListener("click", function(){
+    // dropMenu.classList.remove("hidden");
+    dropMenu.classList.remove("translate-y-[100vh]");
+    dropMenu.classList.toggle("translate-y-[0vh]");
+})
+
+dropMenu.addEventListener("click", function(){
+    dropMenu.classList.add("translate-y-[100vh]");
+    dropMenu.classList.remove("translate-y-[0vh]");
 })
